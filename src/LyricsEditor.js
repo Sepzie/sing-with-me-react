8190943f5377b46db71d64d253248076395986d0
Sepzie <81197023+Sepzie@users.noreply.github.com>
2022-09-04 23:54:15 -0700

    Added file system functionality

--- src/LyricsEditor.js
+++ src/LyricsEditor.js
@@ -1,7 +1,42 @@
 import React, { useState } from 'react';
 
 
+async function getNewFileHandle() {
+  const options = {
+    types: [
+      {
+        description: 'Text Files',
+        accept: {
+          'text/plain': ['.txt'],
+        },
+      },
+      {
+        description: 'Lyrics Files',
+        accept: {
+          'application/octet-stream': ['.lrc']
+        }
+      },
+    ],
+  };
+  const handle = await window.showSaveFilePicker(options);
+  return handle
+}
 
+async function writeFile(fileHandle, contents) {
+  // Create a FileSystemWritableFileStream to write to.
+  const writable = await fileHandle.createWritable();
+  // Write the contents of the file to the stream.
+  await writable.write(contents);
+  // Close the file and write the contents to disk.
+  await writable.close();
+}
+
+async function readFile() {
+  var [fileHandle] = await window.showOpenFilePicker();
+  const file = await fileHandle.getFile();
+  const contents = await file.text();
+  return [fileHandle, contents]
+}
 
 
 function processLyrics(rawLyrics) {
@@ -25,13 +60,20 @@ function processLyrics(rawLyrics) {
 }
 
 
+
 class EditText extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: '' };
+    this.state = {
+      value: 'prefilled text',
+      fileHandle: null
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.load = this.load.bind(this);
+    this.save = this.save.bind(this);
+    this.saveAs = this.saveAs.bind(this);
   }
 
   handleChange(event) {
@@ -44,14 +86,49 @@ class EditText extends React.Component {
     event.preventDefault();
   }
 
+  async load() {
+    var [fileHandle, contents] = await readFile()
+    console.log(contents)
+    this.setState({
+      fileHandle: fileHandle,
+      value: contents
+    })
+  }
+
+  save() {
+    const fileHandle = this.state.fileHandle
+    const contents = this.state.value
+    writeFile(fileHandle, contents)
+  }
+
+
+  saveAs() {
+    var handle = getNewFileHandle()
+    handle.then((result) => {
+      writeFile(result, this.state.value)
+      this.setState({
+        fileHandle: result
+      })
+    })
+  }
+
+
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>Lyrics With Timestamp</label>
-        <textarea type="text" value={this.state.value} onChange={this.handleChange} />
-        <br />
-        <input type="submit" value="Submit" />
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <p><label>Lyrics With Timestamp</label></p>
+          <textarea type="text" value={this.state.value} onChange={this.handleChange}>
+
+          </textarea>
+          <br />
+          <input type="submit" value="Submit" />
+
+        </form>
+        <button onClick={this.load}>Load</button>
+        <button onClick={this.save}>Save</button>
+        <button onClick={this.saveAs}>Save As...</button>
+      </div>
     );
   }
 }
